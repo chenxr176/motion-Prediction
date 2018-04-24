bc30cc1ecfdc574337f0ce6ab95c3c4b10a762bd
ChenXiangru <383471246@qq.com>
2018-04-24 21:15:58 +0800

    Add another file

--- attention-MotionPrediction.py
+++ attention-MotionPrediction.py
@@ -6,11 +6,12 @@ PAD = 0
 EOS = 1
 vocab_size = 10
 input_embedding_size = 20
-encoder_hidden_units = 20
-decoder_hidden_units = 20
+encoder_hidden_units = 256
+decoder_hidden_units = 256
 batch_size = 20
 n_input = 132
 fc1 = 256
+sequenceLength = 50
 
 
 def random_sequences(length_from, length_to, vocab_lower, vocab_upper, batch_size):
@@ -23,14 +24,14 @@ def random_sequences(length_from, length_to, vocab_lower, vocab_upper, batch_siz
         yield [
             np.random.randint(low=vocab_lower, high=vocab_upper, size=random_length()).tolist()
             for _ in range(batch_size)
-            ]
+        ]
 
 
 batches = random_sequences(length_from=3, length_to=10,
                            vocab_lower=2, vocab_upper=10,
                            batch_size=batch_size)
 
-
+'''
 def make_batch(inputs, max_sequence_length=None):
     sequence_lengths = [len(seq) for seq in inputs]
     batch_size = len(inputs)
@@ -42,8 +43,7 @@ def make_batch(inputs, max_sequence_length=None):
             inputs_batch_major[i, j] = element
     inputs_time_major = inputs_batch_major.swapaxes(0, 1)
     return inputs_time_major, sequence_lengths
-
-
+'''
 
 '''
     wb16 = {
@@ -93,11 +93,9 @@ def make_batch(inputs, max_sequence_length=None):
 
 train_graph = tf.Graph()
 with train_graph.as_default():
-
-    encoder_inputs_raw = tf.placeholder(shape=(None,None),dtype= tf.float32,name = 'inputs_raw')
-    decoder_inputs_raw = tf.placeholder(shape=(None,None),dtype=tf.float32,name = 'decoder_inputs_raw')
-    decoder_targets_raw = tf.placeholder(shape=(None,None),dtype=tf.float32,name = 'decoder_targets_raw')
-
+    encoder_inputs_raw = tf.placeholder(shape=(None, None, None), dtype=tf.float32, name='inputs_raw')
+    decoder_inputs_raw = tf.placeholder(shape=(None, None, None), dtype=tf.float32, name='decoder_inputs_raw')
+    decoder_targets_raw = tf.placeholder(shape=(None, None, None), dtype=tf.float32, name='decoder_targets_raw')
 
     encoder_inputs = tf.placeholder(shape=(None, None), dtype=tf.int32, name='encoder_inputs')
     decoder_inputs = tf.placeholder(shape=(None, None), dtype=tf.int32, name='decoder_inputs')
@@ -153,72 +151,62 @@ with train_graph.as_default():
         'biases': tf.Variable(tf.constant(0.1, shape=[fc1, ], dtype=tf.float32), dtype=tf.float32),
     }
 
-    x_in = tf.matmul(encoder_inputs_raw, wb11['weights']) + wb11['biases']
+    x_in = tf.einsum('ijk,kl->ijl', encoder_inputs_raw, wb11['weights'])
     # x_in = tf.sigmoid(x_in)
-    x_in = tf.matmul(x_in, wb12['weights']) + wb12['biases']
+    x_in = tf.einsum('ijk,kl->ijl', x_in, wb12['weights'])
     # x_in = tf.sigmoid(x_in)
-    x_in = tf.matmul(x_in, wb13['weights']) + wb13['biases']
+    x_in = tf.einsum('ijk,kl->ijl', x_in, wb13['weights'])
     x_in = tf.sigmoid(x_in)
     # x_in = tf.sigmoid(x_in)
-    x_in = tf.matmul(x_in, wb14['weights']) + wb14['biases']
+    x_in = tf.einsum('ijk,kl->ijl', x_in, wb14['weights'])
     # x_in = tf.sigmoid(x_in)
-    x_in = tf.matmul(x_in, wb15['weights']) + wb15['biases']
+    x_in = tf.einsum('ijk,kl->ijl', x_in, wb15['weights'])
     x_in = tf.sigmoid(x_in)
-
-
 
     encoder_inputs_fc = x_in
 
-    attention = tf.Variable(tf.zeros([input_embedding_size,input_embedding_size]), dtype=tf.float32)
+    attention = tf.Variable(tf.zeros([fc1, fc1]), dtype=tf.float32)
 
-    embeddings = tf.Variable(tf.random_uniform([vocab_size, input_embedding_size], -1.0, 1.0), dtype=tf.float32)
+    # embeddings = tf.Variable(tf.random_uniform([vocab_size, input_embedding_size], -1.0, 1.0), dtype=tf.float32)
 
-
-
-
-
-    encoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, encoder_inputs)
+    # encoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, encoder_inputs)
 
     encoder_cell = tf.contrib.rnn.LSTMCell(encoder_hidden_units)
     encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(
-        encoder_cell, encoder_inputs_embedded,
+        encoder_cell, encoder_inputs_fc,
         dtype=tf.float32, time_major=True,
     )
-    N = tf.einsum('ijk,kl->ijl', encoder_outputs,attention)
+    N = tf.einsum('ijk,kl->ijl', encoder_outputs, attention)
 
+    # decoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, decoder_inputs)
 
+    # encoder_outputs
 
-
-
-    decoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, decoder_inputs)
-
-
-
-
-    #encoder_outputs
-
-    n = tf.Variable(tf.zeros([1,100,20],dtype=tf.float32),trainable=False,dtype=tf.float32)
+    # n = tf.Variable(tf.zeros([1,sequenceLength,132],dtype=tf.float32),trainable=False,dtype=tf.float32)
     decoder_cell = tf.contrib.rnn.LSTMCell(decoder_hidden_units)
-    #att_d_e=decoder_inputs_embedded+N
-    att_d_e = decoder_inputs_embedded+tf.concat([N,n],0)
+    # att_d_e=decoder_inputs_embedded+N
+    att_d_e = decoder_inputs_raw + N
     decoder_outputs, decoder_final_state = tf.nn.dynamic_rnn(
         decoder_cell, att_d_e,
         initial_state=encoder_final_state,
         dtype=tf.float32, time_major=True, scope="plain_decoder",
     )
 
-    decoder_logits = tf.contrib.layers.linear(decoder_outputs, vocab_size)
-    decoder_prediction = tf.argmax(decoder_logits, 2)
-    stepwise_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
-        labels=tf.one_hot(decoder_targets, depth=vocab_size, dtype=tf.float32),
-        logits=decoder_logits,
-    )
-    loss = tf.reduce_mean(stepwise_cross_entropy)
+    # decoder_logits = tf.contrib.layers.linear(decoder_outputs, vocab_size)
+    # decoder_prediction = tf.argmax(decoder_logits, 2)
+    # stepwise_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+    #    labels=tf.one_hot(decoder_targets, depth=vocab_size, dtype=tf.float32),
+    #    logits=decoder_logits,
+    # )
+    pred = tf.rashape(decoder_outputs, [-1, n_input])
+
+    label = tf.reshape(decoder_targets_raw, [-1, n_input])
+
+    loss = tf.reduce_mean(tf.pow(pred - label, 2))
     train_op = tf.train.AdamOptimizer().minimize(loss)
 
 loss_track = []
 epochs = 3000
-
 
 with tf.Session(graph=train_graph) as sess:
     sess.run(tf.global_variables_initializer())
@@ -234,8 +222,8 @@ with tf.Session(graph=train_graph) as sess:
         loss_track.append(l)
         if epoch == 0 or epoch % 1000 == 0:
             print('loss: {}'.format(sess.run(loss, feed_dict)))
-            predict_ = sess.run(decoder_prediction, feed_dict)
-            for i, (inp, pred) in enumerate(zip(feed_dict[encoder_inputs].T, predict_.T)):
+            predict_ = sess.run(pred, feed_dict)
+            for i, (inp, pred) in enumerate(zip(feed_dict[encoder_inputs].T, pred.T)):
                 print('input > {}'.format(inp))
                 print('predicted > {}'.format(pred))
                 if i >= 20:
